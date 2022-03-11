595789c36642dd906baf5667dbe6287a1c1383ef
slowlorisx2 <robertojoseqc@gmail.com>
2022-03-10 21:16:58 -0500

    day4

---
+++ htmlBasics/DAY4/app.js
@@ -0,0 +1,8 @@
+  alert("You have JS enable on this browser");
+  //Single line comment here
+  /*Multiple lines
+  Comments here
+  as you can see*/
+document.write(navigator.appName);
+document.write("<br><h2>This is injected via JS code</h2>");
+document.write(navigator.appVersion);

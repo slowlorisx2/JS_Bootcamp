51e60aa7572d244e79790e2fecbe7655a253c13f
Roberto <robertojoseqc@gmail.com>
2022-03-17 23:43:59 -0400

    day7

---
+++ htmlBasics/DAY7/app.js
@@ -0,0 +1,22 @@
+// alert("Thank you so much! Just to confirm your cc number is: " + prompt("Dear user, this site request your credit card information", "Enter your credit card number here..."));
+// alert("Based on your answer, The results are: " + confirm("Confirm if you you have tested positive for COVID on the past 2 years") + ".");
+
+// var x = 4.20;
+// var y = 9;
+
+// console.log("4 + 9 = " + (x + y));
+
+// Function that adds two numbers
+function suma(x, y){
+    let numA = x;
+    let numB = y;
+    let total = x + y;
+    return total;
+}
+
+const total = suma(90, 5);
+
+function myFunction(){
+    alert("The sum of 90 and 5 is " + total + ".");
+}
+myFunction();

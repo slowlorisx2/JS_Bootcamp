b7e963224275576d99dca376d01f28148faeec88
slowlorisx2 <robertojoseqc@gmail.com>
2022-03-22 18:04:58 -0400

    day 10

---
+++ htmlBasics/Week3/DAY9/class/main.js
@@ -0,0 +1,24 @@
+// function addNumbers (arg1, arg2) {
+//     var result = parseFloat(arg1) + parseFloat(arg2);
+//     return result;
+//   }
+
+
+// alert("Please enter a numerical value in each of the following prompt dialog boxes. \nThe two numbers will be added together.");
+
+//     var num1 = prompt("Please enter a numerical value.","");
+//     var num2 = prompt("Please enter a numerical value.","");
+    
+//     document.write("<p>The result of the addNumbers() function is: <strong>" + addNumbers(num1, num2) + "</strong></p>");
+
+
+// about "this"
+// var color = "red";
+
+// function whatIsThis(){
+//     var colorInner = "blue";
+//     alert(this);
+//     alert(this.colorInner);
+// }
+
+// whatIsThis();

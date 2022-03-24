0c6b0b10d03ed2f8a81015fcddc99223593257b7
slowlorisx2 <robertojoseqc@gmail.com>
2022-03-23 22:53:59 -0400

    day 11

---
+++ htmlBasics/Week3/DAY10/app.js
@@ -0,0 +1,50 @@
+// //Prototype, call
+
+// function Person(fisrtarg, lastarg, agearg, eyeColorarg){
+//     this.firstName = fisrtarg;
+//     this.lastName = lastarg;
+//     this.age = agearg;
+//     this.eyeColor = eyeColorarg;
+// }
+
+// function AboutMe(){
+//     alert(mePerson);
+//     alert(mePerson.firstName);
+//     alert(mePerson.lastName);
+//     alert(mePerson.age);
+//     alert(mePerson.eyeColor);
+// }
+
+// Person.prototype.about = function() {
+//     alert(this);
+//     alert(this.firstName);
+//     alert(this.lastName);
+//     alert(this.age);
+//     alert(this.eyeColor);
+// }
+
+// const mePerson = new Person(prompt("Enter your first name"), prompt("Enter your last name"), prompt("Enter your age"), prompt("Enter your eye color"));
+// mePerson.about ();
+
+// console.log(mePerson.age);
+
+
+// function alertMe(){
+//     alert(sumMe(1, 2));
+// }
+
+// function sumMe(num1, num2){
+//     return num1 +num2;
+// }
+
+// Anonymous Function
+// var sumMe = function(num1, num2){
+//     return num1 + num2;
+// }
+
+// Arrow function
+//  var sumMe = (num1, num2) => num1 + num2;
+
+
+// alertMe();
+
---
+++ htmlBasics/Week3/Day11- Debuging/app.js
@@ -0,0 +1,33 @@
+/* Types of errors
+- Logic errors
+- Run-time errors
+- Load time errors
+*/
+
+// var a =   prompt("Enter a number", "");
+// var b =   prompt("Enter a second number", "");
+// var c =   prompt("Enter a third number", "");
+// var d =   prompt("Enter a fourth number", "");
+// var e =   prompt("Enter a fifth number", "");
+
+// var solution = a*b+c+d*e;
+
+// console.log(`watchpoint 1: ${a} ${b} ${c} ${d} ${e} `);
+// document.write("The total you are looking for is a*b+c+d*e = " + solution);
+
+/***************************************************
+ Mistakes in code cost time and money.  Watch your
+ scripts!
+***************************************************/
+function mathTime() {
+var a = 2;
+var b = 2;
+var c = 2;
+
+
+var solution = a*b+c;
+
+
+document.write("The total you are looking for is a*b+c = " + solution.toFixed(2));
+
+}
